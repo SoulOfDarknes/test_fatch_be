212925e050baf8b32d845481d77055ea473219bc
Vasyl <ve4nuy@rambler.ru>
2024-02-18 01:41:14 +0200

    change imports

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,31 +1,39 @@
-import { Controller, Get, Query, Res, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
+import {
+  Controller,
+  Get,
+  Query,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 
 @Controller('api')
 export class ApiController {
-  private readonly requestCountPerSecondLimit = 50;
+  private requestCountPerSecondLimit = 50;
   private requestsCounter = 0;
   private lastResetTime = Date.now();
 
   @Get()
-  async handleRequest(@Query('index') index: number, @Res() res: Response) {
+  async handleRequest(@Query('index') index: number): Promise<number> {
     const currentTime = Date.now();
+
     if (currentTime - this.lastResetTime > 1000) {
       this.requestsCounter = 0;
       this.lastResetTime = currentTime;
     }
 
     if (this.requestsCounter >= this.requestCountPerSecondLimit) {
-      return res
-        .status(HttpStatus.TOO_MANY_REQUESTS)
-        .json({ error: 'Too many requests' });
+      throw new HttpException(
+        'Too many requests',
+        HttpStatus.TOO_MANY_REQUESTS,
+      );
     }
 
     this.requestsCounter++;
 
-    const delay = Math.floor(Math.random() * 1000) + 1;
-    setTimeout(() => {
-      res.json({ index });
-    }, delay);
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.floor(Math.random() * 1000) + 1),
+    );
+
+    return index;
   }
 }
